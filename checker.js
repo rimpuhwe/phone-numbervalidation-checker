1187a9d4889c776084537b69d905373078d0235f
Raissa Micheline <rimpuhwemanzi@gmail.com>
2024-09-17 13:13:46 +0200

    mapping the html with js

---
+++ checker.js
@@ -0,0 +1,4 @@
+const enteredInput = document.getElementById('user-input');
+const checkerBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultpage = document.getElementById('results-div');
