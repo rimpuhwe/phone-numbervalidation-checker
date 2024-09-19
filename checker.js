a20ed46a4aecc2e70e5c51deda446e0e9043fc0c
Raissa Micheline <rimpuhwemanzi@gmail.com>
2024-09-19 08:37:35 +0200

    check the validation

--- checker.js
+++ checker.js
@@ -2,3 +2,30 @@ const enteredInput = document.getElementById('user-input');
 const checkerBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const resultpage = document.getElementById('results-div');
+
+
+const regexRule = (word) =>{
+    const regex = /^1?\s?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$/gi;
+    return regex.test(word);
+}
+checkerBtn.addEventListener('click', () =>{
+    const number = '' + enteredInput.value;
+
+    if (enteredInput.value == ''){
+        alert('insert your phone number please');
+        return;
+    }
+    else if(regexRule(number)){
+        resultpage.innerHTML = `
+        <p class="green">valid number:<br>${number}</p>   `
+    }
+    else{
+        resultpage.innerHTML = `
+        <p class="red">invalid number:<br>${number}</p>`
+    }
+})
+
+clearBtn.addEventListener('click',()=>{
+    resultpage.innerHTML="";
+    enteredInput.value = "";
+})
